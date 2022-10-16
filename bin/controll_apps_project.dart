4b0c4db4cb91dea602edaa5d5d3c134dbbe374de
abdusatarov33 <abdusatarovfaruh3@gmail.com>
2022-10-16 22:32:07 +0600

    mesage

--- bin/controll_apps_project.dart
+++ bin/controll_apps_project.dart
@@ -2,10 +2,5 @@ import 'package:controll_apps_project/controll_apps_project.dart'
     as controll_apps_project;
 
 void main(List<String> arguments) {
-  AppsUrls appsUrls = AppsUrls();
   print('Hello world: ${controll_apps_project.calculate()}!');
-}
-
-class AppsUrls {
-  static String ecoCityBaseUrl = 'TESTURL1';
 }
