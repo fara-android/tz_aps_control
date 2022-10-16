66a5521fda16ceea330dfdac3a7efc8ab6322aa7
abdusatarov33 <abdusatarovfaruh3@gmail.com>
2022-10-16 22:30:22 +0600

    wip

--- bin/controll_apps_project.dart
+++ bin/controll_apps_project.dart
@@ -2,6 +2,7 @@ import 'package:controll_apps_project/controll_apps_project.dart'
     as controll_apps_project;
 
 void main(List<String> arguments) {
+  AppsUrls appsUrls = AppsUrls();
   print('Hello world: ${controll_apps_project.calculate()}!');
 }
 
