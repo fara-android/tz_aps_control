f2244be35e581d452986e8cc78a9612f7d9c2ca4
abdusatarov33 <abdusatarovfaruh3@gmail.com>
2022-10-16 22:25:02 +0600

    first commit

---
+++ bin/controll_apps_project.dart
@@ -0,0 +1,10 @@
+import 'package:controll_apps_project/controll_apps_project.dart'
+    as controll_apps_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${controll_apps_project.calculate()}!');
+}
+
+class AppsUrls {
+  static String ecoCityBaseUrl = 'TESTURL1';
+}
---
+++ test/controll_apps_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:controll_apps_project/controll_apps_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
